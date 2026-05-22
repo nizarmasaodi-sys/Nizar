4d24cb19932ff4957cf5b72449e43e4b777ec295
nizarmasaodi-sys <nizarmasaodi@gmail.com>
2026-05-22 05:22:06 +0300

    Add chat functionality with server communication
    Implement a chat interface that sends messages to a server and displays replies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,62 @@
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ChatPage(),
+    );
+  }
+}
+
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  TextEditingController controller = TextEditingController();
+  String reply = "";
+
+  Future<void> sendMessage() async {
+    var url = Uri.parse('https://chat-server-48n4.onrender.com/chat');
+    var response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'message': controller.text}),
+    );
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      setState(() {
+        reply = data['reply'];
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Chat")),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(controller: controller),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: sendMessage, child: Text("إرسال")),
+            SizedBox(height: 20),
+            Text(reply),
+          ],
+        ),
+      ),
+    );
+  }
+}
